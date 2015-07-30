7b92938fc399b1d9407448ca3fb4de620dfd63f1
James Miller <jrmiller84@gmail.com>
2015-07-30 14:38:00 -0400

    Adding initial code and example README.md.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # go-ifttt-maker
 A simple Go library for interacting with the IFTTT Maker Channel.
+
+    //Create an instance of our Maker struct
+    var maker = new(GoIFTTTMaker.MakerChannel)
+    var key = "your ifttt maker key"
+    var event = "your ifttt maker event"
+
+    //Set the values to send
+    maker.Value1 = ""
+    maker.Value2 = ""
+    maker.Value3 = ""
+    maker.Value4 = ""
+
+    //Send our request
+    maker.Send(key, event)
---
+++ main.go
@@ -0,0 +1,47 @@
+package GoIFTTTMaker
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+//MakerChannel assists in the sending of POST requests to the IFTTT Maker Channel.
+type MakerChannel struct {
+	Value1 string `json:"value1"`
+	Value2 string `json:"value2"`
+	Value3 string `json:"value3"`
+	Value4 string `json:"value4"`
+}
+
+//Send a request to the IFTTT Maker Channel.
+func (maker *MakerChannel) Send(key string, event string) bool {
+
+	api := fmt.Sprintf("https://maker.ifttt.com/trigger/%v/with/key/%v", event, key)
+	body, err := json.Marshal(maker)
+
+	if err != nil {
+		panic(err)
+	}
+
+	buffer := bytes.NewBuffer([]byte(body))
+	req, err := http.NewRequest("POST", api, buffer)
+
+	if err != nil {
+		panic(err)
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	return resp.StatusCode == 200
+}
